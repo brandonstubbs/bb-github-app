60c83533123fac893b88a20d24938f488cf63968
Brandon Stubbs <brandon@bos-solutions.co.uk>
2022-07-06 14:25:06 -0400

    create example

---
+++ README.md
@@ -0,0 +1,51 @@
+# Babashka Github Application
+
+This is an example [babashka](https://github.com/babashka) script that can
+authenticate as a Github application. To demo this, the application interacts
+with the Github Checks API. As the main focus of this is for the authentication
+and interaction with the Checks API we are not plugging on a complete webhook
+demo, you could use what you have learned from here and follow the [official
+Github tutorial](https://docs.github.com/en/developers/apps/guides/creating-ci-tests-with-the-checks-api)
+
+
+## Prerequisites
+You will need two things: \
+A repository and a commit on that repository which we will create the check runs
+on. \
+A Github Application and private key to use for authentication.
+
+### Github Application Setup
+#### Create Application
+To create a Github Application you need to do the following:
+- In your Github Settings, navigate to: Developer settings -> Github Apps.
+- Click "New Github App"
+  - Choose a unique Github App name. For this example I am using `bb checks`
+  - Homepage URL. Link to your application webpage. For this example I am using
+    `https://babashka.org/`
+  - Webhook -> active. Disable this, not in scope for this demo.
+  - Repository Permissions -> Enable `Read and write` for `Checks`.
+  - Click "Create GitHub App"
+  - Take note of the `App ID` under the about section, you will need to copy
+    this into the code.
+
+#### Generate Application Private Key
+To create a Github Application private key, you will need to do the following:
+- In your Github Settings, navigate to: Developer settings -> Github Apps.
+- Click "Edit" next to the Github app your are using for this.
+- Scroll down to "Private keys"
+- click "Generate"
+- Take note of this downloaded pem, you will need to copy the location of this
+  into the code.
+
+#### Install Github app to your account
+You have created a Github application that can be used by other accounts, now
+you just need to install it into yours.
+- In your Github Settings, navigate to: Developer settings -> Github Apps.
+- Click "Edit" next to the Github app your are using for this.
+- Click "Public page" in the sidebar.
+- Click "Install".
+
+
+## Run Example
+Open [github_app.clj](github_app.clj) and navigate to the [Rich Comment Block](https://betweentwoparens.com/blog/rich-comment-blocks/#rich-comment).
+Change the variables under "change these" and step through the examples.
---
+++ github_app.clj
@@ -0,0 +1,190 @@
+#!/usr/bin/env bb
+(ns github-app
+  (:require [clojure.string              :as str]
+            [babashka.pods               :as pods]
+            [babashka.curl               :as curl]
+            [cheshire.core               :as json]
+            [pod.babashka.buddy.sign.jwt :as jwt]
+            [pod.babashka.buddy.keys     :as keys])
+  (:import (java.time Instant)
+           (java.time.temporal ChronoUnit)))
+
+
+
+;; we need the buddy pod to create the application access-key
+(pods/load-pod 'org.babashka/buddy "0.2.0")
+
+
+
+(def base-uri "https://api.github.com")
+
+
+
+(defn ->path
+  [& strs]
+  (str/join "/" strs))
+
+
+
+(defn from-json
+  [string]
+  (json/parse-string string true))
+
+
+
+(defn app-installation-token
+  [app-id private-key]
+  (jwt/sign
+    {:iss app-id
+     ;; 1 minute in the past to allow for clock drift
+     :iat (-> (Instant/now) (.minus 1 ChronoUnit/MINUTES) .getEpochSecond)
+     ;; JWT expiration time - 10 minutes it maximum
+     :exp (-> (Instant/now) (.plus 10 ChronoUnit/MINUTES) .getEpochSecond)}
+    (keys/private-key private-key)
+    {:alg :rs256}))
+
+
+
+(defn installation-id
+  [installation-token gh-owner]
+  (let [{:keys [status body]}
+        (curl/get "https://api.github.com/app/installations"
+          {:headers {"Authorization" (str "Bearer " installation-token)
+                     "Accept"        "application/vnd.github.v3+json"}})]
+    (when (<= 200 status 299)
+      (->> body
+        from-json
+        (filter #(= gh-owner (-> % :account :login)))
+        first
+        :id))))
+
+
+
+(defn installation-access-token
+  [installation-token installation-id]
+  (let [{:keys [status body]}
+        (curl/post (->path base-uri "app/installations" installation-id "access_tokens")
+          {:headers {"Authorization" (str "Bearer " installation-token)
+                     "Accept"        "application/vnd.github.v3+json"}})]
+    (when (<= 200 status 299)
+      (-> body from-json :token))))
+
+
+
+(defn create-check-run
+  [gh-owner repo access-token payload]
+  (let [{:keys [status body]}
+        (curl/post (->path base-uri "repos" gh-owner repo "check-runs")
+          {:headers {"Authorization" (str "token " access-token)
+                     "Accept"        "application/vnd.github.v3+json"}
+           :body (json/generate-string payload)})]
+    (when (<= 200 status 299)
+      (-> body from-json :id))))
+
+
+
+(defn update-check-run
+  [gh-owner repo access-token check-run-id payload]
+  (curl/patch (->path base-uri "repos" gh-owner repo "check-runs" check-run-id)
+    {:headers {"Authorization" (str "token " access-token)
+               "Accept"        "application/vnd.github.v3+json"}
+     :body (json/generate-string payload)}))
+
+
+
+(defn- check-runs-helper
+  [gh-owner repo check-run-id]
+  (->path "https://github.com" gh-owner repo "runs" check-run-id))
+
+
+
+(comment
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;                                                                          ;;
+  ;;                 ----==| C H A N G E   T H E S E |==----                  ;;
+  ;;                                                                          ;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Github App ID from applicaiton you created, e.g 123456
+  (def app-id "<app-id>")
+  ;; Github account owner, organisation or person. e.g brandonstubbs
+  (def gh-owner "<github-owner>")
+  ;; repo you want to test this on, e.g bb-github-app
+  (def repo "<repo>")
+  ;; Path to Github App private key, e.g /tmp/bb-checks.pem
+  (def private-key "<private-pem-path>")
+  ;; Commit you want to create the check runs on, e.g TODO
+  (def commit "<commit>")
+
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;                                                                          ;;
+  ;;                        ----==| D E M O |==----                           ;;
+  ;;                                                                          ;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; we create a signed access-token for our Github app.
+  (def installation-token (app-installation-token app-id private-key))
+  ;; we need to find the correct installation-id for the target account.
+  (def installation-id (installation-id installation-token gh-owner))
+  ;; we need to create an access-token on behalf of the github app to the target
+  ;; account.
+  (def access-token (installation-access-token installation-token installation-id))
+
+
+  ;; -- example of creation -> success --
+
+  ;; we create a new chuck run on the target commit, this one we will evolve
+  ;; into a success
+  (def check-run-id
+    (create-check-run gh-owner repo access-token
+      {:name     "will succeed"
+       :head_sha commit
+       :status   "queued"
+       :output   {:title   "Queued"
+                  :summary "## Job Summary \n some markdown of the summary"}}))
+
+  ;; view your check run here:
+  (check-runs-helper gh-owner repo check-run-id)
+
+  ;; we update that check run to show that it is running.
+  (update-check-run gh-owner repo access-token check-run-id
+    {:status "in_progress"
+     :output {:title   "In Progress"
+              :summary "## Job Summary \n I am busy running..."}})
+  ;; You will see the check run updated.
+
+  ;; we update that check run to a success.
+  (update-check-run gh-owner repo access-token check-run-id
+    {:status "completed"
+     :conclusion "success"
+     :output {:title   "Completed"
+              :summary "## Job Summary \n I ran successfully..."}})
+
+
+  ;; -- example of creation -> failure --
+  (def check-run-id
+    (create-check-run gh-owner repo access-token
+      {:name     "will fail"
+       :head_sha commit
+       :status   "queued"
+       :output   {:title   "Queued"
+                  :summary "## Job Summary \n some markdown of the summary"}}))
+
+  ;; view your check run here:
+  (check-runs-helper gh-owner repo check-run-id)
+
+  ;; we update that check run to show that it is running.
+  (update-check-run gh-owner repo access-token check-run-id
+    {:status "in_progress"
+     :output {:title   "In Progress"
+              :summary "## Job Summary \n I am busy running..."}})
+  ;; You will see the check run updated.
+
+  ;; we update that check run to a success.
+  (update-check-run gh-owner repo access-token check-run-id
+    {:status     "completed"
+     :conclusion "failure"
+     :output     {:title   "Completed"
+                  :summary "## Job Summary \n I failed..."}})
+  ;; You will see the check run updated.
+  )
